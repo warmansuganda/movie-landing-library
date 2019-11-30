5ac9ef873476e5f9fc898f4b8b710168c3a13e17
Warman Suganda <warman.suganda@gmail.com>
2019-11-30 23:35:41 +0700

    make migration for table lendings

---
+++ database/migrations/2019_11_30_161618_create_table_lending.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableLending extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lendings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('movie_id');
+            $table->unsignedBigInteger('member_id');
+            $table->date('lending_date');
+            $table->date('returned_date')->nullable();
+            $table->float('lateness_charge')->nullable();
+            $table->timestamps();
+
+            // relation table
+            $table->foreign('movie_id')
+            ->references('id')->on('movies')
+            ->onDelete('cascade');
+
+            $table->foreign('member_id')
+            ->references('id')->on('members')
+            ->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lendings');
+    }
+}
